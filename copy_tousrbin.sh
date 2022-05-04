0c29f42f797f90da9dfe44fedb84aa6635def149
rollieolie <56703680+rollieolie@users.noreply.github.com>
2022-05-04 13:42:35 -0500

    Create copy_tousrbin.sh

---
+++ copy_tousrbin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp subs.py /usr/bin/subs;
+chmod +x /usr/bin/subs;
+cp crtypy.py /usr/bin/crtypy;
+chmod +x crtypy
+cp crtsh.sh /usr/bin/crtsh;
+chmod +x crtsh;
+
+echo "Programs are copied to your /usr/bin"
